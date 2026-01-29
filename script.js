cf3d4c53c994b2432473b9e5b63102a57332f95c
Farhaan <farhaan.ca>
2026-01-29 15:03:56 -0500

    day 3 beginning

--- script.js
+++ script.js
@@ -52,4 +52,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // initial setup shortly after load
   setTimeout(setupStars, 50);
+
+  // pointermove -> update CSS variables on body for reactive backgrounds
+  document.body.addEventListener("pointermove", (e) => {
+    const { currentTarget: el, clientX: x, clientY: y } = e;
+    const { top: t, left: l, width: w, height: h } = el.getBoundingClientRect();
+    el.style.setProperty('--posX', x - l - w / 2);
+    el.style.setProperty('--posY', y - t - h / 2);
+  });
 });
